653a9115c0fde091a39cedfed97a1d30f33d9e9c
123ZOKzok <zippyondiba@gmail.com>
2022-08-26 17:00:29 +0300

    Done

--- js/challenge.js
+++ js/challenge.js
@@ -1,61 +1,109 @@
-const incrementCount =document.getElementById("plus")
-    const decrementCount =document.getElementById("minus")
-    const counter = document.getElementById('counter')
-    const heart = document.getElementById("heart")
-    const pause= document.getElementById("pause")
-   const  submit = document.getElementById("comment-form")
-   let count=0;
-    function myFunc ()
-    c= setInterval(()=>{
-    
-    
-    counter.innerHTML++;
-},1000)
-    //function to increment the counter
-    const handleIncrement = ()=> {
-        count++;
-        counter.innerHTML=count;
-    }
-    //function to decrement te counter
-    const handleDecrement = () => {
-        count--;
-        counter.innerHTML=count;
-    }
-    const pauseTime = () => {
-        if(pause.innerHTML!= 'play'){
-            clearInterval(c)
-            pause.innerHTML='pause'
-        }
-    }
-    let num= 0;
-    function likes(){
-        let like = document.querySelector(".likes")
-        let likeul = document.createElement("ul")
-        let likeli = document.createElement("li")
-        let currentCount = counter.innerHTML;
-        if (num>=0){
-            num++;
-            likeli.innerHTML=`${currentCount} has been liked ${num} time`;
-            like.append(likeul);
-            likeul.append(likeli);
-        }
-        }
-        function submitsButton(e){
-            let comment = document.querySelector(".comments");
-            let commentInput = document.querySelector('#comment-input').value;
-            let p = document.createElement("p");
-            e.preventDefault();
-            p.innerHTML= commentInput;
-            comment.append(p);
+// increments counter every second
+let isCounting = true
 
+// document.addEventListener("DOMContentLoaded", function(){
+//     let counter = document.querySelector('#counter')
+//     setInterval(function() { 
+//         countinglet currentCount = parseInt(counter.innerHTML, 10)
+//         counter.textContent = currentCount + 1 
+//     }, 1000)
+// })
+document.addEventListener("DOMContentLoaded", function(){
+    let counter = document.querySelector('#counter')
+    setInterval(function() { 
+        if (isCounting === true){
+            let currentCount = parseInt(counter.innerHTML, 10)
+            counter.textContent = currentCount + 1
+        } 
+    }, 1000)
+})
 
-        }
-    
-    
-    document.addEventListener("DOMContentLoaded",myFunc)
-    
-    incrementCount.addEventListener("click",handleIncrement)
-    decrementCount.addEventListener("click",handleDecrement)
-    pause.addEventListener('click', pauseTime);
-     heart.addEventListener('click',likes);
-    submit.addEventListener('submit',submitsButton);
+// increment/decrement on button clicks
+function minus(){
+    let currentCount = parseInt(counter.innerHTML, 10)
+    counter.textContent = currentCount - 1
+}
+function plus(){
+    let currentCount = parseInt(counter.innerHTML, 10)
+    counter.textContent = currentCount + 1
+}
+document.querySelector('#minus').addEventListener("click", function() {
+    minus()
+})
+document.querySelector('#plus').addEventListener("click", function() {
+    plus()
+})
+
+// logs "like" for the currentCount, which update with # of likes
+
+const heartLikes = {}
+
+function addLikes(){
+    let currentCount = parseInt(counter.innerHTML, 10)
+    let countTimes
+    let exist = document.getElementById(`${currentCount}count`)
+    if (exist) {
+        heartLikes.currentCount += 1
+        likesPhrase(exist, currentCount, heartLikes.currentCount)
+    } else {
+        countTimes = 0
+        let ul = document.querySelector('.likes')
+        let li = document.createElement('li')
+        li.className = 'likeLog'
+        li.id = `${currentCount}count`
+        countTimes += 1
+        heartLikes.currentCount = countTimes
+        console.log(heartLikes.currentCount)
+        likePhrase(li, currentCount, heartLikes.currentCount)
+        ul.append(li)
+    }
+}
+function likePhrase(name, currentCount, countTimes){
+    name.textContent = `${currentCount} has been liked ${countTimes} time`
+}
+function likesPhrase(name, currentCount, countTimes){
+    name.textContent = `${currentCount} has been liked ${countTimes} times`
+}
+document.querySelector('#heart').addEventListener("click", function(){
+    addLikes()
+})
+
+// pause which pauses counter, disables all buttons, label change to "resume"
+// on second click, resumes count, enables all buttons, label changes back to "pause"
+
+function pauseCount(){
+    isCounting = !isCounting
+
+    if (isCounting === false){
+        disableBtns()
+        document.getElementById('pause').textContent = "resume"
+    } else {
+        enableBtns()
+        document.getElementById('pause').textContent = "pause"
+    }
+}
+function disableBtns(){
+    document.getElementById('minus').disabled = true
+    document.getElementById('plus').disabled = true
+    document.getElementById('heart').disabled = true
+    document.getElementById('submit').disabled = true
+}
+function enableBtns(){
+    document.getElementById('minus').disabled = false
+    document.getElementById('plus').disabled = false
+    document.getElementById('heart').disabled = false
+    document.getElementById('submit').disabled = false
+}
+document.querySelector('#pause').addEventListener("click", function(){
+    pauseCount()
+})
+
+// comments can be left
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let comment = e.target.comment.value
+    let logComment = document.createElement('p')
+    logComment.textContent = comment
+    document.querySelector('#list').appendChild(logComment)
+})
